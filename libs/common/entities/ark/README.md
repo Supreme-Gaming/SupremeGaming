349d99e3ef7aa6ebca0fc45d6d4b07846f65c66e
Edher <imacleopard2010@gmail.com>
2020-09-06 21:32:06 -0500

    Added base player, tribes, and servers entities

---
+++ libs/common/entities/ark/README.md
@@ -0,0 +1,7 @@
+# common-entities-ark
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test common-entities-ark` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/common/entities/steam/src/lib/all/all.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+import { GuidIdentity } from '@supremegaming/common/entities';
+
+@Entity({ name: 'steam_profiles' })
+export class SteamProfile extends GuidIdentity {
+  @PrimaryColumn()
+  public steamId: number;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public url: string;
+
+  @Column()
+  public avatar: string;
+
+  @Column()
+  public communityBanned: boolean;
+
+  @Column()
+  public vacBanned: boolean;
+
+  @Column()
+  public vacBanCount: number;
+
+  @Column()
+  public gameBanCount: number;
+
+  @Column()
+  public lastBanned: number;
+}
