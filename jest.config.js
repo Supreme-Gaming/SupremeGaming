4264f560f7974c8a6e237d0349e07601fc88f78d
Edgar <imacleopard2010@gmail.com>
2021-10-16 20:28:00 -0500

    Added public pages lib
    This library will contain mostly routing confiruation and feature modules for the public-facing website pages.

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
-  projects: '<rootDir>/libs/ui',
+  projects: '<rootDir>/libs/pages/public',
 };
