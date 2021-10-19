22d5321d98a5df8ccb1eda4761841a6d38fc40f5
Edgar <imacleopard2010@gmail.com>
2021-10-19 03:00:51 -0500

    Added boilerplate common interfaces lib
    Lib will contain website envionment interfaces for now, but will be a lib for all common interfaces across the monorepo

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
-  projects: '<rootDir>/libs/common/ngx',
+  projects: '<rootDir>/libs/common/interfaces',
 };
