f176b58a287ff7db2e7ab5b27c1e174e1491b21c
Edgar <imacleopard2010@gmail.com>
2021-10-19 02:19:39 -0500

    Added boilerplate data-access lib

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
-  projects: '<rootDir>/libs/pages/public',
+  projects: '<rootDir>/libs/data-access',
 };
