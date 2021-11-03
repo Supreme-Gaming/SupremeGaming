7345f8645c9a007fe4d4448d52615482c03c0a3f
Edgar <imacleopard2010@gmail.com>
2021-11-02 20:45:35 -0500

    Comleted tribe details copmonent
    Implemented in ark explorer tribe details module

--- libs/ui/src/lib/modules/player-tribe-explorer/components/tribe-details/tribe-details.component.ts
+++ libs/ui/src/lib/modules/player-tribe-explorer/components/tribe-details/tribe-details.component.ts
@@ -1,12 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, pluck, shareReplay, switchMap, withLatestFrom } from 'rxjs/operators';
+
+import { GameServerPlayer, GameServerTribe } from '@supremegaming/common/interfaces';
+import { TribesService } from '@supremegaming/data-access';
+
+import { AbstractRoutableResourceComponent } from '../abstract-routable-resource/abstract-routable-resource.component';
 
 @Component({
   selector: 'supremegaming-tribe-details',
   templateUrl: './tribe-details.component.html',
-  styleUrls: ['./tribe-details.component.scss'],
+  styleUrls: ['../player-details/player-details.component.scss', './tribe-details.component.scss'],
 })
-export class TribeDetailsComponent implements OnInit {
-  constructor() {}
+export class TribeDetailsComponent extends AbstractRoutableResourceComponent implements OnInit {
+  @Input()
+  public tribeGuid: Observable<string>;
 
-  ngOnInit(): void {}
+  @Input()
+  public game: string;
+
+  public tribe: Observable<GameServerTribe>;
+  public tribeOwner: Observable<GameServerPlayer>;
+  public tribeMembers: Observable<Array<GameServerPlayer>>;
+
+  constructor(private readonly ts: TribesService) {
+    super();
+  }
+
+  public ngOnInit(): void {
+    this.tribe = this.tribeGuid.pipe(
+      switchMap((guid) => {
+        return this.ts.searchTribesForGame(this.game, guid).pipe(pluck<GameServerTribe[], GameServerTribe>('0'));
+      }),
+      shareReplay()
+    );
+
+    this.tribeMembers = this.tribe.pipe(pluck('Members'));
+
+    this.tribeOwner = this.tribeMembers.pipe(
+      withLatestFrom(this.tribe.pipe(pluck('OwnerId'))),
+      map(([members, ownerId]) => {
+        return members.find((m) => {
+          return m.Id === ownerId;
+        });
+      })
+    );
+  }
 }
