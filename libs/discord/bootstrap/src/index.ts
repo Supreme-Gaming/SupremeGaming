3b30e5200e4d3360b0733a6c85c01cd7442ac340
Edgar Hernandez <imacleopard2010@gmail.com>
2022-10-22 05:09:47 -0500

    Add discord bootstrap module and deps

---
+++ libs/discord/bootstrap/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/bootstrapper/discord-bootstrap';
---
+++ libs/discord/bootstrap/src/lib/feature-module/feature-module.ts
@@ -0,0 +1,105 @@
+import {
+  CacheType,
+  GuildMember,
+  Interaction,
+  Message,
+  MessageReaction,
+  PartialMessage,
+  PartialMessageReaction,
+  PartialUser,
+  User,
+} from 'discord.js';
+
+import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from '@discordjs/builders';
+
+export abstract class OnReady {
+  abstract clientOnReady(): void | Promise<void>;
+}
+
+export abstract class OnInteractionCreate {
+  abstract clientOnInteractionCreate(interaction: Interaction<CacheType>): void | Promise<void>;
+}
+
+export abstract class OnGuildMemberAdd {
+  abstract clientOnGuildMemberAdd(member: GuildMember): void | Promise<void>;
+}
+
+export abstract class OnMessageReactionAdd {
+  abstract clientOnMessageReactionAdd(
+    reaction: MessageReaction | PartialMessageReaction,
+    user: User | PartialUser
+  ): void | Promise<void>;
+}
+
+export abstract class OnMessageReactionRemove {
+  abstract clientOnMessageReactionRemove(
+    reaction: MessageReaction | PartialMessageReaction,
+    user: User | PartialUser
+  ): void | Promise<void>;
+}
+
+export abstract class OnMessageCreate {
+  abstract clientOnMessageCreate(message: Message): void | Promise<void>;
+}
+
+export abstract class OnMessageUpdate {
+  abstract clientOnMessageUpdate(
+    oldMessage: Message | PartialMessage,
+    newMessage: Message | PartialMessage
+  ): void | Promise<void>;
+}
+
+export abstract class OnMessageDelete {
+  abstract clientOnMessageDelete(message: Message | PartialMessage): void | Promise<void>;
+}
+
+export abstract class OnError {
+  abstract clientOnError(error: Error): void | Promise<void>;
+}
+
+export abstract class OnWarn {
+  abstract clientOnWarn(message: string): void | Promise<void>;
+}
+
+export abstract class SlashCommands {
+  abstract commands(): SlashCommandTypes;
+}
+
+export interface DiscordFeatureModuleConstructor {
+  new (): DiscordFeatureModule;
+}
+
+export type SlashCommandTypes = Array<SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | any>;
+
+export class DiscordFeatureModule {
+  public commands?(): SlashCommandTypes;
+
+  public clientOnReady?: () => void | Promise<void>;
+
+  public clientOnInteractionCreate?: (interaction: Interaction<CacheType>) => void | Promise<void>;
+
+  public clientOnGuildMemberAdd?: (member: GuildMember) => void | Promise<void>;
+
+  public clientOnMessageReactionAdd?: (
+    reaction: MessageReaction | PartialMessageReaction,
+    user: User | PartialUser
+  ) => void | Promise<void>;
+
+  public clientOnMessageReactionRemove?: (
+    reaction: MessageReaction | PartialMessageReaction,
+    user: User | PartialUser
+  ) => void | Promise<void>;
+
+  public clientOnMessageCreate?: (messsage: Message) => void | Promise<void>;
+
+  public clientOnMessageUpdate?: (
+    oldMessage: Message | PartialMessage,
+    newMessage: Message | PartialMessage
+  ) => void | Promise<void>;
+
+  public clientOnMessageDelete?: (messsage: Message | PartialMessage) => void | Promise<void>;
+
+  public clientOnError?: (error: Error) => void | Promise<void>;
+
+  public clientOnWarn?: (message: string) => void | Promise<void>;
+}
