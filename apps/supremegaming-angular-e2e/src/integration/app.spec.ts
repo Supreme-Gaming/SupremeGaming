a6108341aadfae939a97eda6f75a71a92c4a23d0
Edgar Hernandez <imacleopard2010@gmail.com>
2022-11-25 00:43:34 -0600

    Migrate last e2e app to eslint
    Remove tslint and codelizer as deps

--- apps/supremegaming-angular-e2e/src/integration/app.spec.ts
+++ apps/supremegaming-angular-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('supremegaming-angular', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to supremegaming-angular!');
+    getGreeting().contains('Welcome supremegaming-angular');
   });
 });
