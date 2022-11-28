07ac25c41953570f896a38e97d4eb42f0629d826
Edgar Hernandez <imacleopard2010@gmail.com>
2022-11-28 04:49:54 -0600

    Add selection review component and disclosure

---
+++ libs/ui/src/lib/modules/shop/components/donation-selections-review/donation-selections-review.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'supremegaming-donation-selections-review',
+  templateUrl: './donation-selections-review.component.html',
+  styleUrls: ['./donation-selections-review.component.scss'],
+})
+export class DonationSelectionsReviewComponent {}
