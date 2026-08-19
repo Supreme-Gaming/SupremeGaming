ec03ff67e5d27a819423da28163062d616b7bdd3
Proantagonist <imacleopard2010@gmail.com>
2026-08-19 16:58:49 +0000

    remove defunct ark-cacher-node app

--- apps/ark-cacher-node/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-};
-
-export { productionConfig as ormconfig } from '../environments/ormconfig';
--- apps/ark-cacher-node/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-};
-
-export { localConfig as ormconfig } from '../environments/ormconfig';
